1d51ee71116c8629546c6cff3b4e13bd55ce9d97
Lucas Medeiros <lucastoc@gmail.com>
2016-12-14 18:28:14 -0300

    add logged in filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    unless session[:user_id].present?
+      flash[:alert] = 'You are not logged in'
+      redirect_to new_authentication_path
+    end
+  end
 end
